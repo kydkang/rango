67734d6c133ec5d0a09e31286fa289a66f5d1e51
kydkang@gmail.com <kydkang@users.noreply.github.com>
2023-02-07 08:16:50 +0900

    After Main

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+
+    class Meta:
+        model = Category
+        fields = ('name', )
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text='Please enter the title of the page.')
+    url = forms.URLField(max_length=200, help_text='Please enter the URL of the page.')
+
+    class Meta:
+        model = Page
+        exclude = ('category', )
